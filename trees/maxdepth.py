ec0648d05c0668d93b7b26fed818daea56fc2cfc
Rushikesh Sawant <rushikeshsawant58@gmail.com>
2025-04-24 22:26:24 -0400

    "Initial Commit"

---
+++ trees/maxdepth.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxDepth(self, root):
+        q = deque()
+        if root:
+            q.append(root)
+
+        level = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
