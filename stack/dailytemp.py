19d68df76f75a66833f5ec9f6e5abef94bb1a091
Rushikesh Sawant <rushikeshsawant58@gmail.com>
2025-05-29 15:46:22 -0400

    Initial commit

---
+++ stack/dailytemp.py
@@ -0,0 +1,17 @@
+class Solution():
+    def dailytemp(self,lil):
+        stack = []
+        res = [0] * len(lil)
+
+
+        for i , t in enumerate(lil):
+            while stack and t>stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t,i))
+        return res
+
+if __name__ == '__main__' :
+    sol = Solution()
+    lil = [73,74,75,71,69,72,76,73]
+    print(sol.dailytemp(lil))
